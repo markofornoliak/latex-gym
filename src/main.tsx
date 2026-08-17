5110d3f38ef33f0e5762b16e496ea61c3764f568
markofornoliak <130382040+markofornoliak@users.noreply.github.com>
2026-08-17 12:13:35 +0200

    Apply curriculum editorial enhancements before rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
+import './data/editorialEnhancements';
 import { App } from './app/App';
 import './styles/index.css';
 import './styles/fidelity.css';
