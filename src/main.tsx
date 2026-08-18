4059294f59b66a0dd9ccbc74286334be8bec7beb
markofornoliak <130382040+markofornoliak@users.noreply.github.com>
2026-08-18 11:20:27 +0200

    style: load compiler workspace design

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import './styles/projects.css';
 import './styles/referenceDeep.css';
 import './styles/progressDeep.css';
 import './styles/interactionPolish.css';
+import './styles/compilerWorkspace.css';
 
 registerSW({ immediate: true });
 createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
