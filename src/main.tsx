69ee57a46ddbac594e9a1cddff5adb4be673e646
markofornoliak <130382040+markofornoliak@users.noreply.github.com>
2026-08-18 11:34:17 +0200

    style: load daily training dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ import './styles/referenceDeep.css';
 import './styles/progressDeep.css';
 import './styles/interactionPolish.css';
 import './styles/compilerWorkspace.css';
+import './styles/trainingDashboard.css';
 
 registerSW({ immediate: true });
 createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
